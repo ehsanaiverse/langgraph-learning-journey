18b745c19e96868ce29d90f187f83b61008f36a0
ehsanaiverse <ihsan42180@gmail.com>
2026-07-11 15:32:31 +0500

    Connect with LangSmith for tracing

--- 5_chatbot/chatbot_frontend.py
+++ 5_chatbot/chatbot_frontend.py
@@ -90,7 +90,11 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
         
-    CONFIG = {"configurable": {"thread_id": st.session_state["thread_id"]}}
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {"thread_id": st.session_state["thread_id"]},
+        "run_name": "chat_run"
+        }
     
     # first add the message to message history
     with st.chat_message("assistant"):
